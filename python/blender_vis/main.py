ef75376da3b2544767c467045fbb59517283cf3f
GiveMeTheReason <botva2000@gmail.com>
2022-10-25 16:08:52 +0300

    small render fix

--- python/blender_vis/main.py
+++ python/blender_vis/main.py
@@ -1,7 +1,14 @@
-import bpy
+import math
 import numpy as np
 
-def add_mesh(name, verts, faces=None, edges=None, col_name="Collection"):  
+import bpy
+
+
+def deg2rad(angle):
+    return angle * math.pi / 180
+
+
+def add_mesh(name, verts, faces=None, edges=None, col_name="Collection"):
     if edges is None:
         edges = []
     if faces is None:
@@ -23,42 +30,40 @@ def add_mesh(name, verts, faces=None, edges=None, col_name="Collection"):
 def create_log(data, num_of_sensors, vel, period):
     layer_num = 0
     verts = []
-    for item in data:
+    for layer_num, item in enumerate(data):
         for i, dist in enumerate(item):
             x = vel * period * layer_num
-            y = dist * np.sin(deg2rad(15 + 30 * i))
-            z = dist * np.cos(deg2rad(15 + 30 * i))
+            y = dist * math.sin(deg2rad(15 + 30 * i))
+            z = dist * math.cos(deg2rad(15 + 30 * i))
             verts.append([x, y, z])
-        layer_num += 1
 
-
-    faces = []
-    faces.append(list(range(0, num_of_sensors)))
+    faces = list(range(0, num_of_sensors))
     for i in range(len(verts) - 1):
         current_point_id = i
         next_point_id = current_point_id + 1
-        if next_point_id % 12 == 0:
+        if next_point_id % num_of_sensors == 0:
             next_point_id -= num_of_sensors
 
         next_layer_point_id = current_point_id + num_of_sensors
         next_layer_next_point_id = next_layer_point_id + 1
-        if next_layer_next_point_id % 12 == 0:
+        if next_layer_next_point_id % num_of_sensors == 0:
             next_layer_next_point_id -= num_of_sensors
 
-        
         if next_layer_point_id < len(verts):
-            faces.append([current_point_id, next_point_id, next_layer_next_point_id, next_layer_point_id])
+            faces.append([
+                current_point_id,
+                next_point_id,
+                next_layer_next_point_id,
+                next_layer_point_id
+            ])
 
     faces.append(list(range(len(verts) - num_of_sensors, len(verts))))
 
     add_mesh("points", verts, faces)
 
-def deg2rad(angle):
-    return angle * np.pi / 180
 
 data = np.loadtxt("/home/s/Desktop/skoltech/design_factory/blender_vis/data/groundtruth.txt")
 num_of_sensors = 24
 vel = 1
 period = 0.1
 create_log(data, num_of_sensors, vel, period)
-
